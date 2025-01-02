24e4d9981025cff146fa924ee1b5777380e6b93f
guineto07 <146396354+guineto07@users.noreply.github.com>
2025-01-02 15:31:57 -0300

    Update sp_CartaoTransacoes_CRUD_2024_12_31.sql

--- sp_CartaoTransacoes_CRUD_2024_12_31.sql
+++ sp_CartaoTransacoes_CRUD_2024_12_31.sql
@@ -1,43 +1,43 @@
 /*================================================================================================================================================================
     AUTOR: Guilherme Neto
-    DATA DE CRIA«√O: 31/12/2024
-    DESCRI«√O: 
-        Esta procedure permite realizar operaÁıes CRUD na tabela Cartao_Transacoes, a exclus„o È feita de forma lÛgica (soft delete),
-        consultas de transaÁıes com base nos par‚metros Id_Cliente, Numero_Cartao, intevalo de Data_Transacao e pelo Valor_Transacao.
-		Para as aÁıes INSERT, UPDATE e DELETE, È necess·rio que o cliente e o cart„o correspondente j· esteja cadastrado na tabela Clientes.
+    DATA DE CRIA√á√ÉO: 31/12/2024
+    DESCRI√á√ÉO: 
+        Esta procedure permite realizar opera√ß√µes CRUD na tabela Cartao_Transacoes, a exclus√£o √© feita de forma l√≥gica (soft delete),
+        consultas de transa√ß√µes com base nos par√¢metros Id_Cliente, Numero_Cartao, intevalo de Data_Transacao e pelo Valor_Transacao.
+	Para as a√ß√µes INSERT, UPDATE e DELETE, √© necess√°rio que o cliente e o cart√£o correspondente j√° esteja cadastrado na tabela Clientes.
 		
-    PAR¬METROS:
-        @Acao VARCHAR(7)          - Especifica a aÁ„o a ser realizada: 'INSERT', 'UPDATE', 'DELETE', 'SELECT'.
-        @Id_Transacao INT         - Identificador da transaÁ„o a ser modificada (necess·rio para UPDATE e DELETE).
-        @Id_Cliente INT           - Identificador do cliente (utilizado nas aÁıes INSERT, UPDATE, DELETE e SELECT). Chave estrangeira com a tabela Clientes.
-        @Numero_Cartao VARCHAR(16) - N˙mero do cart„o associado ‡ transaÁ„o.
-        @Valor_Transacao DECIMAL(12, 2) - Valor da transaÁ„o.
-        @Descricao VARCHAR(100)   - DescriÁ„o detalhada da transaÁ„o.
-        @Data_Inicial DATETIME    - Data inicial para filtro de transaÁıes (usado na consulta SELECT) e considerado como Data_Transacao nas aÁıes INSERT e UPDATE.
-        @Data_Final DATETIME      - Data final para filtro de transaÁıes (usado na consulta SELECT).
+    PAR√ÇMETROS:
+        @Acao VARCHAR(7)          - Especifica a a√ß√£o a ser realizada: 'INSERT', 'UPDATE', 'DELETE', 'SELECT'.
+        @Id_Transacao INT         - Identificador da transa√ß√£o a ser modificada (necess√°rio para UPDATE e DELETE).
+        @Id_Cliente INT           - Identificador do cliente (utilizado nas a√ß√µes INSERT, UPDATE, DELETE e SELECT). Chave estrangeira com a tabela Clientes.
+        @Numero_Cartao VARCHAR(16) - N√∫mero do cart√£o associado √† transa√ß√£o.
+        @Valor_Transacao DECIMAL(12, 2) - Valor da transa√ß√£o.
+        @Descricao VARCHAR(100)   - Descri√ß√£o detalhada da transa√ß√£o.
+        @Data_Inicial DATETIME    - Data inicial para filtro de transa√ß√µes (usado na consulta SELECT) e considerado como Data_Transacao nas a√ß√µes INSERT e UPDATE.
+        @Data_Final DATETIME      - Data final para filtro de transa√ß√µes (usado na consulta SELECT).
 
-    ERROS POSSÕVEIS:
-        - Caso @Id_Transacao n„o seja fornecido para operaÁıes de atualizaÁ„o ou exclus„o, um erro ser· gerado.
-        - Caso o @Id_Cliente n„o exista na tabela Clientes, um erro ser· gerado nas operaÁıes de 'INSERT', 'UPDATE' ou 'DELETE'.
-        - Se ocorrer uma falha durante a execuÁ„o de qualquer operaÁ„o, a transaÁ„o ser· revertida (rollback).
+    ERROS POSS√çVEIS:
+        - Caso @Id_Transacao n√£o seja fornecido para opera√ß√µes de atualiza√ß√£o ou exclus√£o, um erro ser√° gerado.
+        - Caso o @Id_Cliente n√£o exista na tabela Clientes, um erro ser√° gerado nas opera√ß√µes de 'INSERT', 'UPDATE' ou 'DELETE'.
+        - Se ocorrer uma falha durante a execu√ß√£o de qualquer opera√ß√£o, a transa√ß√£o ser√° revertida (rollback).
 
     EXEMPLOS DE USO:
-        -- Inserir uma nova transaÁ„o:
+        -- Inserir uma nova transa√ß√£o:
         EXEC sp_CartaoTransacoes_CRUD @Acao = 'INSERT', @Id_Cliente = 1, 
                                       @Numero_Cartao = '1111111111111111', 
                                       @Valor_Transacao = 100.00, 
                                       @Data_Inicial = '2024-12-31', 
                                       @Descricao = 'Compra no supermercado';
 
-        -- Alterar uma transaÁ„o existente:
+        -- Alterar uma transa√ß√£o existente:
         EXEC sp_CartaoTransacoes_CRUD @Acao = 'UPDATE', @Id_Transacao = 1, 
                                       @Valor_Transacao = 170.85, 
                                       @Descricao = 'Compra no ARAGUAIA shopping';
 
-        -- Excluir uma transaÁ„o (soft delete):
+        -- Excluir uma transa√ß√£o (soft delete):
         EXEC sp_CartaoTransacoes_CRUD @Acao = 'DELETE', @Id_Transacao = 1;
 
-        -- Consultar transaÁıes:
+        -- Consultar transa√ß√µes:
         EXEC sp_CartaoTransacoes_CRUD @Acao = 'SELECT', @Id_Cliente = 1';
 		EXEC sp_CartaoTransacoes_CRUD @Acao = 'SELECT', @Numero_Cartao ='1111111111111111'
 		EXEC sp_CartaoTransacoes_CRUD @Acao = 'SELECT', @Data_Inicial = '2024-01-01', @Data_Final = '2024-12-31';
@@ -47,13 +47,13 @@
         EXEC sp_CartaoTransacoes_CRUD @Acao = 'SELECT', @Numero_Cartao ='1111111111111111', @Data_Inicial = '2024-01-01', @Data_Final = '2025-12-31';
  ====================================================================================================================================================== */
 CREATE PROCEDURE sp_CartaoTransacoes_CRUD
-    @Acao VARCHAR(7), -- AÁıes (INSERT, UPDATE, DELETE, SELECT)
+    @Acao VARCHAR(7), -- A√ß√µes (INSERT, UPDATE, DELETE, SELECT)
     @Id_Transacao INT = NULL, 
     @Id_Cliente INT = NULL,
     @Numero_Cartao VARCHAR(16) = NULL,
     @Valor_Transacao DECIMAL(12, 2) = NULL,
-    @Data_Inicial DATETIME = NULL, -- Par‚metro de data inicial
-    @Data_Final DATETIME = NULL,   -- Par‚metro de data final
+    @Data_Inicial DATETIME = NULL, -- Par√¢metro de data inicial
+    @Data_Final DATETIME = NULL,   -- Par√¢metro de data final
     @Descricao VARCHAR(100) = NULL
 AS
 BEGIN
@@ -62,37 +62,37 @@ BEGIN
     DECLARE @Erro INT = 0;
 
     BEGIN TRY
-        -- Inicia transaÁ„o no banco de dados
+        -- Inicia transa√ß√£o no banco de dados
         BEGIN TRANSACTION;
 
-        -- Insere nova transaÁ„o de cart„o (AÁ„o: INSERT)
-		-- Para esta aÁ„o cpnsiderar o @Data_Inicial como Data_TransaÁ„o
+        -- Insere nova transa√ß√£o de cart√£o (A√ß√£o: INSERT)
+		-- Para esta a√ß√£o cpnsiderar o @Data_Inicial como Data_Transa√ß√£o
         IF @Acao = 'INSERT' 
         BEGIN
-			-- Antes de agir verifica se a transaÁ„o existe
+			-- Antes de agir verifica se a transa√ß√£o existe
 			IF NOT EXISTS (SELECT 1 FROM Clientes WHERE Numero_Cartao = @Numero_Cartao)
             BEGIN
-                THROW 50000, 'N˙mero de cart„o n„o encontrado.', 1;
+                THROW 50000, 'N√∫mero de cart√£o n√£o encontrado.', 1;
             END;
 
             INSERT INTO Cartao_Transacoes (Id_Cliente, Numero_Cartao, Valor_Transacao, Data_Transacao, Descricao)
             VALUES (@Id_Cliente, @Numero_Cartao, @Valor_Transacao, @Data_Inicial, @Descricao);
         END;
 
-        -- Altera transaÁ„o de cart„o (AÁ„o: UPDATE)
-		-- Para esta aÁ„o considerar o paramÍtro @Data_Inicial como Data_TransaÁ„o
+        -- Altera transa√ß√£o de cart√£o (A√ß√£o: UPDATE)
+		-- Para esta a√ß√£o considerar o param√™tro @Data_Inicial como Data_Transa√ß√£o
         IF @Acao = 'UPDATE' 
         BEGIN
             IF @Id_Transacao IS NULL
             BEGIN
                 SET @Erro = 1;
-                THROW 50000, 'Id_Transacao inv·lido, campo obrigatÛrio para atualizaÁ„o.', 1;
+                THROW 50000, 'Id_Transacao inv√°lido, campo obrigat√≥rio para atualiza√ß√£o.', 1;
             END;
 
-			-- Antes de agir verifica se a transaÁ„o existe
+			-- Antes de agir verifica se a transa√ß√£o existe
 			IF NOT EXISTS (SELECT 1 FROM Cartao_Transacoes WHERE Id_Transacao = @Id_Transacao) 
             BEGIN
-                THROW 50000, 'TransaÁ„o n„o encontrada para atualizaÁ„o.', 1;
+                THROW 50000, 'Transa√ß√£o n√£o encontrada para atualiza√ß√£o.', 1;
             END;
 
             UPDATE Cartao_Transacoes
@@ -104,19 +104,19 @@ BEGIN
             WHERE Id_Transacao = @Id_Transacao;
         END;
 
-        -- Soft Delete da transaÁ„o de cart„o - Marca a transaÁ„o como inativa 'I' (AÁ„o: DELETE)
+        -- Soft Delete da transa√ß√£o de cart√£o - Marca a transa√ß√£o como inativa 'I' (A√ß√£o: DELETE)
         IF @Acao = 'DELETE' 
         BEGIN 
             IF @Id_Transacao IS NULL
             BEGIN
                 SET @Erro = 1;
-                THROW 50000, 'Id_Transacao inv·lido, campo obrigatÛrio para exclus„o.', 1;
+                THROW 50000, 'Id_Transacao inv√°lido, campo obrigat√≥rio para exclus√£o.', 1;
             END;
 
-			-- Antes de agir verifica se a transaÁ„o existe
+			-- Antes de agir verifica se a transa√ß√£o existe
             IF NOT EXISTS (SELECT 1 FROM Cartao_Transacoes WHERE Id_Transacao = @Id_Transacao)
             BEGIN
-                THROW 50000, 'TransaÁ„o n„o encontrada para exclus„o.', 1;
+                THROW 50000, 'Transa√ß√£o n√£o encontrada para exclus√£o.', 1;
             END;
 
             UPDATE Cartao_Transacoes
@@ -124,7 +124,7 @@ BEGIN
             WHERE Id_Transacao = @Id_Transacao;
         END;
 
-        -- Consulta transaÁıes por diversos par‚metros (AÁ„o: SELECT)
+        -- Consulta transa√ß√µes por diversos par√¢metros (A√ß√£o: SELECT)
         IF @Acao = 'SELECT' 
         BEGIN
             SELECT Id_Transacao, Id_Cliente, Numero_Cartao, Valor_Transacao, Data_Transacao, Descricao, Status
@@ -132,16 +132,16 @@ BEGIN
             WHERE (@Id_Cliente IS NULL OR Id_Cliente = @Id_Cliente)
               AND (@Numero_Cartao IS NULL OR Numero_Cartao = @Numero_Cartao)
               AND (@Valor_Transacao IS NULL OR Valor_Transacao = @Valor_Transacao)
-              AND (@Data_Inicial IS NULL AND @Data_Final IS NULL OR Data_Transacao BETWEEN @Data_Inicial AND @Data_Final) -- PerÌodo entre duas datas
-              AND Status = 'A' -- SÛ transaÁıes ativas
+              AND (@Data_Inicial IS NULL AND @Data_Final IS NULL OR Data_Transacao BETWEEN @Data_Inicial AND @Data_Final) -- Per√≠odo entre duas datas
+              AND Status = 'A' -- S√≥ transa√ß√µes ativas
         END;
 
-        -- Se tudo correr bem, comita a transaÁ„o no banco de dados
+        -- Se tudo correr bem, comita a transa√ß√£o no banco de dados
         COMMIT TRANSACTION;
     END TRY
 
     BEGIN CATCH
-        -- Em caso de erro, realiza o rollback da transaÁ„o no banco de dados
+        -- Em caso de erro, realiza o rollback da transa√ß√£o no banco de dados
         ROLLBACK TRANSACTION;
         SELECT ERROR_MESSAGE() AS ErrorMessage;
     END CATCH
